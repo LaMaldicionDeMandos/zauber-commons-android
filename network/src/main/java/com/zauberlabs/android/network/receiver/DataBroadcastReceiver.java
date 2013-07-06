c655ee188400f307f8392fa14c837df2be20897a
Hernan Zalazar <hernanzalazar@gmail.com>
2013-07-05 22:54:05 -0300

    Add onReceive impl.

--- network/src/main/java/com/zauberlabs/android/network/receiver/DataBroadcastReceiver.java
+++ network/src/main/java/com/zauberlabs/android/network/receiver/DataBroadcastReceiver.java
@@ -1,17 +1,35 @@
 package com.zauberlabs.android.network.receiver;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Parcelable;
 
 /**
  * Created by hernan on 5/7/13.
  */
-public class DataBroadcastReceiver<T extends Parcelable> {
+public class DataBroadcastReceiver<T extends Parcelable> extends BroadcastReceiver implements EventReceiver {
 
+    public static final String DATA_PAYLOAD_KEY = "DATA";
     private Command<T> command;
     private Event event;
 
     public DataBroadcastReceiver(Command<T> command, Event event) {
         this.command = command;
         this.event = event;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        T payload = getPayloadAsData(intent);
+        command.execute(context, payload);
+    }
+
+    public static <T extends Parcelable> T getPayloadAsData(Intent intent) {
+        return intent.getParcelableExtra(DATA_PAYLOAD_KEY);
     }
 }
--- network/src/test/java/com/zauberlabs/android/network/receiver/DataBroadcastReceiverTest.java
+++ network/src/test/java/com/zauberlabs/android/network/receiver/DataBroadcastReceiverTest.java
@@ -1,5 +1,7 @@
 package com.zauberlabs.android.network.receiver;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Parcelable;
 
 
@@ -7,26 +9,40 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.robolectric.RobolectricTestRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by hernan on 5/7/13.
  */
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(RobolectricTestRunner.class)
 public class DataBroadcastReceiverTest {
 
-    @Mock
     private Command<Parcelable> command;
-
-    @Mock
     private Event event;
+    private Intent intent;
+    private Context context;
+    private Parcelable payload;
 
     private DataBroadcastReceiver receiver;
 
     @Before
     public void setUp() throws Exception {
+        command = mock(Command.class);
+        event = mock(Event.class);
+        intent = mock(Intent.class);
+        context = mock(Context.class);
+        payload = mock(Parcelable.class);
+        when(intent.getParcelableExtra(anyString())).thenReturn(payload);
         receiver = new DataBroadcastReceiver(command, event);
     }
 
@@ -34,5 +50,22 @@ public class DataBroadcastReceiverTest {
     public void shouldBuildReceiverWithCommandAndEvent() {
         DataBroadcastReceiver receiver = new DataBroadcastReceiver(command, event);
         assertNotNull(receiver);
+    }
+
+    @Test
+    public void shouldReturnEventWhenAsked() {
+        assertEquals(event, receiver.getEvent());
+    }
+
+    @Test
+    public void shouldObtainPayloadFromIntentOnReceive() {
+        receiver.onReceive(context, intent);
+        verify(intent).getParcelableExtra(anyString());
+    }
+
+    @Test
+    public void shouldCallCommandWithPayload() {
+        receiver.onReceive(context, intent);
+        verify(command).execute(eq(context), eq(payload));
     }
 }

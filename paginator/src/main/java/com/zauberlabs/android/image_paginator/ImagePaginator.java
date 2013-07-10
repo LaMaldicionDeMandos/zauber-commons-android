f54b2c78ffcb459d53dc8c5c8c4895d4135487f7
Marcelo Pasut <marcelopasut@zauberlabs.com>
2013-07-10 16:15:22 -0300

    add pagitator functionality

--- paginator/src/main/java/com/zauberlabs/android/image_paginator/ImagePaginator.java
+++ paginator/src/main/java/com/zauberlabs/android/image_paginator/ImagePaginator.java
@@ -1,6 +1,7 @@
 package com.zauberlabs.android.image_paginator;
 
 import android.content.Context;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -13,6 +14,8 @@ import android.widget.RadioGroup;
  * Created by marcelo on 7/10/13.
  */
 public class ImagePaginator extends FrameLayout implements ViewPager.OnPageChangeListener {
+    private static final int MARGIN = 4;
+
     private ViewPager pager;
     private RadioGroup container;
     private RadioButton[] buttons;
@@ -37,6 +40,39 @@ public class ImagePaginator extends FrameLayout implements ViewPager.OnPageChang
         pager.setOnPageChangeListener(this);
         container = (RadioGroup)view.findViewById(R.id.container);
         this.addView(view);
+    }
+
+    public void setAdapter(final PagerAdapter adapter){
+        addBullets(adapter);
+        pager.setAdapter(adapter);
+    }
+
+    private void addBullets(final PagerAdapter adapter){
+        container.removeAllViews();
+        if(checkNotEmpty(adapter)){
+            buttons = new RadioButton[adapter.getCount()];
+            buttons[0] = createBullet(true);
+            for(int i=1;i<buttons.length;i++){
+                buttons[i] = createBullet(false);
+            }
+        }
+    }
+
+    private RadioButton createBullet(final boolean checked){
+        RadioButton button = new RadioButton(getContext());
+        RadioGroup.LayoutParams params = new RadioGroup.LayoutParams(RadioGroup.LayoutParams.WRAP_CONTENT,RadioGroup.LayoutParams.WRAP_CONTENT);
+        params.setMargins(MARGIN, MARGIN, MARGIN, MARGIN);
+        button.setClickable(false);
+        button.setLayoutParams(params);
+        //TODO esto va a ser externalizado
+        //button.setButtonDrawable(R.drawable.page_bullet);
+        button.setChecked(checked);
+        container.addView(button);
+        return button;
+    }
+
+    private boolean checkNotEmpty(final PagerAdapter adapter){
+        return adapter.getCount() > 0;
     }
 
     @Override

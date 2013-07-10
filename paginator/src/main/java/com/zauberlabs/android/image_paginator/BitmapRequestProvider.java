f34281308687b10413e44d0210b0420baa7512eb
Marcelo Pasut <marcelopasut@zauberlabs.com>
2013-07-10 18:04:57 -0300

    change load to Aquery method

--- paginator/src/main/java/com/zauberlabs/android/image_paginator/BitmapRequestProvider.java
+++
@@ -1,9 +0,0 @@
-package com.zauberlabs.android.image_paginator;
-
-/**
- * Created by marcelo on 7/10/13.
- */
-public interface BitmapRequestProvider {
-    void provide(String url, BitmapRequestListener listener);
-    void cancel();
-}

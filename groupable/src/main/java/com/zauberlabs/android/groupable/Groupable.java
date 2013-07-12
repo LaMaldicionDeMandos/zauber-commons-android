e9e3205cbe186811bcc87501f5ef860a1e76d420
Marcelo Pasut <marcelopasut@zauberlabs.com>
2013-07-12 10:34:41 -0300

    create project and groupable interface

---
+++ groupable/src/main/java/com/zauberlabs/android/groupable/Groupable.java
@@ -0,0 +1,38 @@
+package com.zauberlabs.android.groupable;
+
+import java.util.List;
+
+import android.widget.CompoundButton;
+
+
+/**
+* Representation of a group-like widget.
+*
+* @author Matias Dumrauf
+* @param <T>
+* @since Mar 15, 2013
+*/
+public interface Groupable<T extends CompoundButton> {
+    public interface Predicate<F> {
+        String applyToText(F item);
+        <K> K applyToTag(F item);
+    }
+
+    T addItem(String text, Object tag);
+
+    T addItem(String text, Object tag, boolean checked);
+
+    T findWithTag(Object tag);
+
+    int size();
+
+    boolean isEmpty();
+
+    void clear();
+
+    List<T> getAllChecked();
+
+    <F> void populate(List<F> items, Predicate<F> predicate);
+
+    void setChecked(Object tag, boolean checked);
+}
